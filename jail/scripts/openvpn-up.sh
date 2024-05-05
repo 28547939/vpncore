914f8bd7ce90c07f3aa4bf702132d457fac8ed92
28547939 <122396215+28547939@users.noreply.github.com>
2024-05-05 14:57:03 +0000

    added jail/scripts/openvpn-up.sh, previously in gitignore; readme tweaks

---
+++ jail/scripts/openvpn-up.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -o nounset
+set -x
+
+#echo args $@
+
+export ifconfig_local=$1
+export ifconfig_netmask=$2
+export route_vpn_gateway=$3
+export dev=$4
+export LOCAL_VPN_DIR=$5
+export NAME=$6
+
+if [ ! -z $route_vpn_gateway ] && [ ! -z $ifconfig_local ]; then 
+    ifconfig $dev inet $ifconfig_local $route_vpn_gateway \
+        netmask $ifconfig_netmask broadcast 255.255.255.255
+fi
+
+
+route delete 0.0.0.0/1
+route delete 128.0.0.0/1
+
+route delete default 
+route add default $route_vpn_gateway
+route add 192.168.0.0/16 192.168.21.4 
+
+sh /etc/extvpn-ipfw.sh
+
+env > /home/openvpn/last-up-env.txt
