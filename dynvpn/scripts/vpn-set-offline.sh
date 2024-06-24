30c465cb68950674eb087683c390b50db15a7b25
28547939 <122396215+28547939@users.noreply.github.com>
2024-06-24 01:40:58 +0000

    vpn-set-offline.sh: wait for killed process before returning

--- dynvpn/scripts/vpn-set-offline.sh
+++ dynvpn/scripts/vpn-set-offline.sh
@@ -7,7 +7,11 @@ NAME=$1
 LOCAL_ADDR=$2
 LOCAL_VPN_DIR=$3
 
-ssh -i ~/.ssh/id.openvpn -o StrictHostKeyChecking=off openvpn@$LOCAL_ADDR killall openvpn
+ssh="ssh -i ~/.ssh/id.openvpn -o StrictHostKeyChecking=off openvpn@$LOCAL_ADDR"
+
+$ssh killall openvpn
 
 PIDFILE=$LOCAL_VPN_DIR/state/openvpn-$NAME.pid 
+$ssh wait $(cat $PIDFILE)
+
 rm -f $PIDFILE
